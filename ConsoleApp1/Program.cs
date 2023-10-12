b6c8b9c5ef0c7b6288f245f466d8461605bf8161
KirilKuril <nixto6000@gmail.com>
2023-10-12 23:09:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введіть ціну зошита");
+int x= int.Parse(Console.ReadLine());
+Console.WriteLine(" Введіть ціну ручки");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("Введіть кількість зошитів");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(" Введіть кількість ручок");
+int m = int.Parse(Console.ReadLine());
+int notes = x * n;
+int pencil = y * m;
+int suma = notes + pencil;
+
+Console.WriteLine($"Петрику потрібно {suma} гривень. Для  придбання {n} зошитів і {m} ручок. При ціні зошита {x} гривень і ручки {y} гривень");
